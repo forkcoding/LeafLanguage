23b5e22003fc12254cd77a8a58910b95ec462dd6
leafvmaple <leafvmaple@163.com>
2017-12-21 16:22:14 +0900

    Fixed some bugs.

--- Tools/LanguageReader/Reader.py
+++ Tools/LanguageReader/Reader.py
@@ -10,8 +10,10 @@ MAX_LESSON_COUNT = 50
 def voc_reader():
     """Read the vocabulary."""
 
+    voc_count = 0
     voc_dict = {}
-    reg_voc = re.compile("^[0-9]+\. ([a-zA-Z]+) (\[.+\]) (.+)$")
+    reg_voc = re.compile(r"^[0-9]+\. (.+) (\[.+\]) (.+)$")
+    reg_voc_lite = re.compile(r"^[0-9]+\. ([a-zA-Z\-]+) (.+)$")
 
     with open(VOC_PATH, 'r') as voc_file:
 
@@ -20,29 +22,38 @@ def voc_reader():
                 continue
             voc_line = voc_line.replace("\"", "")
             voc_line = voc_line.replace("\n", "")
-            voc_match = reg_voc.match(voc_line)
+            voc_line = voc_line.replace("  ", " ")
+            voc_match = reg_voc.match(voc_line) or reg_voc_lite.match(voc_line)
             if not voc_match:
                 continue
-            voc_dict[voc_match.group(1)] = [voc_match.group(2), voc_match.group(3)]
+
+            if voc_match.lastindex == 2:
+                voc_dict[voc_match.group(1)] = ["", voc_match.group(2)]
+            elif voc_match.lastindex == 3:
+                voc_dict[voc_match.group(1)] = [voc_match.group(2), voc_match.group(3)]
+
+            voc_count = voc_count + 1
+
+        print voc_count
 
     return voc_dict
 
 def json_reader():
     """Output the vocabulary to json file."""
 
+    words_count = 0
     group_list = []
     lesson_list = []
 
-    reg_index = re.compile("[0-9]+\.")
-    reg_word = re.compile("[0-9]+\.\s*([a-zA-Z]+)")
+    reg_word = re.compile(r"[0-9]+\.\s*([a-zA-Z\S]+)")
     voc_dict = voc_reader()
 
     with open(LESSON_PATH, 'r') as lesson_file:
 
         for lesson_line in lesson_file:
-            if not reg_index.match(lesson_line):
-                continue
             lesson_line.strip()
+            lesson_line = lesson_line.replace("\xef", " ")
+            lesson_line = lesson_line.replace("|", " ")
             word_match = reg_word.match(lesson_line)
             if not word_match:
                 continue
@@ -50,10 +61,14 @@ def json_reader():
             if not voc_dict.has_key(lesson_word):
                 continue
 
-            ext = voc_dict[lesson_word][0]
-            meaning = voc_dict[lesson_word][1]
+            words_count = words_count + 1
 
-            lesson_list.append({"Voc": lesson_word, "Ext": ext, "Type": "", "Meanning": meaning, "Time": 0})
+            lesson_list.append({"Type": "",
+                                "Voc": lesson_word,
+                                "Ext": voc_dict[lesson_word][0],
+                                "Meanning": voc_dict[lesson_word][1],
+                                "Time": 0})
+
             if len(lesson_list) >= MAX_LESSON_COUNT:
                 group_list.append(lesson_list)
                 lesson_list = []
@@ -61,6 +76,8 @@ def json_reader():
         lesson_len = len(lesson_list)
         if lesson_len > 0:
             group_list.append(lesson_list)
+
+        print words_count
 
         json_file = open(OUT_PATH, 'w')
         json_file.write(json.dumps(group_list, encoding='utf-8', ensure_ascii=False,
